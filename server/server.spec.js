839fb6f8189e631c5be0c277c2e19d2acc384c11
Cristian <cristiano.rosetti@gmail.com>
2017-01-20 12:24:11 -0600

    server test pass

---
+++ server/server.spec.js
@@ -0,0 +1,18 @@
+const should = require('should')
+const server = require('./server')
+
+describe('Server', () => {
+
+  it('should require a port to start', () => {
+    return server.start({
+      repo: {}
+    }).should.be.rejectedWith(/port/)
+  })
+
+  it('should require a repository to start', () => {
+    return server.start({
+      port: {}
+    }).should.be.rejectedWith(/repository/)
+  })
+
+})
