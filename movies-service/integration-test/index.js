c7bd85a7453a9354c3f725d7c270b09cb1260c40
Cristian <cristiano.rosetti@gmail.com>
2017-01-26 10:34:39 -0600

    fixing integration test url

--- movies-service/integration-test/index.js
+++ movies-service/integration-test/index.js
@@ -1,13 +1,11 @@
 /* eslint-env mocha */
 const supertest = require('supertest')
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
 describe('movies-service', () => {
-
   const api = supertest('http://192.168.99.100:300')
-
   it('returns a 200 for a known user', (done) => {
-
-    api.get('/movies/premiers')
+    api.get('/movies/premieres')
       .expect(200, done)
   })
 })
