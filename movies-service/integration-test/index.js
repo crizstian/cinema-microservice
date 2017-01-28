f739502c96a3d38492d3e5cb09c41996ebe6930e
Cristian <cristiano.rosetti@gmail.com>
2017-01-28 13:01:51 -0600

    movie service test fixed with http2

--- movies-service/integration-test/index.js
+++ movies-service/integration-test/index.js
@@ -1,12 +1,10 @@
 /* eslint-env mocha */
 const supertest = require('supertest')
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
 describe('movies-service', () => {
-
-  const api = supertest('http://192.168.99.100:300')
-
+  const api = supertest('https://192.168.99.100:300')
   it('returns a 200 for a known user', (done) => {
-
     api.get('/movies/premiers')
       .expect(200, done)
   })
