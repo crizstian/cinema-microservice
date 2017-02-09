c4ac889f3b8990bbaa76a3181d29dc7a7f2d5f52
Cristian <cristiano.rosetti@gmail.com>
2017-02-09 12:30:24 -0600

    notification service finished, test passed ok

--- notification-service/src/config/config.js
+++ notification-service/src/config/config.js
@@ -3,10 +3,11 @@ const serverSettings = {
   ssl: require('./ssl')
 }
 
+// as a better practice we can pass this values via env variables
 const smtpSettings = {
   service: 'Gmail',
-  user: 'cristiano.rosetti@gmail.com',
-  pass: 'Cris123@#'
+  user: 'your-email@gmail.com',
+  pass: 'your email password'
 }
 
 module.exports = Object.assign({}, { serverSettings, smtpSettings })
