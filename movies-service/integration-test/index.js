2a88aefc5a99604b7f28df7dfdbabf35f37666df
Cristian <cristiano.rosetti@gmail.com>
2017-02-10 13:39:19 -0600

    fixing movies service https test

--- movies-service/integration-test/index.js
+++ movies-service/integration-test/index.js
@@ -3,9 +3,9 @@ const supertest = require('supertest')
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
 describe('movies-service', () => {
-  const api = supertest('https://192.168.99.100:3000')
+  const api = supertest('https://192.168.99.100')
   it('returns a 200 for a known user', (done) => {
-    api.get('/movies/premiers')
+    api.get('/movies/premieres')
       .expect(200, done)
   })
 })
